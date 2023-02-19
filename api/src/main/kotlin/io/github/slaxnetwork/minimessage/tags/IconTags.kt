73296a21ad19ad77b1d80accaddf79ec37038776
Tech <debitcardzpaypal@gmail.com>
2023-02-19 04:46:37 -0500

    refactor: move icon tag handling out of builder

---
+++ api/src/main/kotlin/io/github/slaxnetwork/minimessage/tags/IconTags.kt
@@ -0,0 +1,30 @@
+package io.github.slaxnetwork.minimessage.tags
+
+import io.github.slaxnetwork.icon.IconRegistry
+import net.kyori.adventure.text.Component
+import net.kyori.adventure.text.minimessage.Context
+import net.kyori.adventure.text.minimessage.tag.Tag
+import net.kyori.adventure.text.minimessage.tag.resolver.ArgumentQueue
+
+class IconTags(
+    private val iconRegistry: IconRegistry
+) {
+    /**
+     * Converts an icon tag into its corresponding unicode character to be
+     * handled by the resource pack.
+     */
+    fun iconTag(args: ArgumentQueue, ctx: Context): Tag {
+        val iconId = args.popOr("The <icon> tag requires an id to be passed.")
+            .value()
+
+        return Tag.selfClosingInserting(
+            Component
+            .text(iconRegistry.mappedIcons[iconId] ?: UNICODE_WARNING_SYMBOL)
+        )
+    }
+
+    companion object {
+        // tmp
+        private const val UNICODE_WARNING_SYMBOL = '당'
+    }
+}
--- src/main/kotlin/io/github/slaxnetwork/SlaxMiniMessageBuilder.kt
+++ src/main/kotlin/io/github/slaxnetwork/SlaxMiniMessageBuilder.kt
@@ -1,33 +1,26 @@
 package io.github.slaxnetwork
 
 import io.github.slaxnetwork.icon.IconRegistry
-import net.kyori.adventure.text.Component
-import net.kyori.adventure.text.minimessage.Context
+import io.github.slaxnetwork.minimessage.tags.IconTags
 import net.kyori.adventure.text.minimessage.MiniMessage
-import net.kyori.adventure.text.minimessage.tag.Tag
-import net.kyori.adventure.text.minimessage.tag.resolver.ArgumentQueue
 import net.kyori.adventure.text.minimessage.tag.resolver.TagResolver
 
 class SlaxMiniMessageBuilder(
-    private val iconRegistry: IconRegistry
+    iconRegistry: IconRegistry
 ) {
+    private val iconTagHandler = IconTags(iconRegistry)
+
+    /**
+     * Create a new [MiniMessage] instance with all TagResolvers.
+     */
     fun createInstance(): MiniMessage {
         val builder = MiniMessage.builder()
 
         builder.tags(TagResolver.resolver(
             TagResolver.standard(),
-            TagResolver.resolver("icon", this::iconTag)
+            TagResolver.resolver("icon", iconTagHandler::iconTag)
         ))
 
         return builder.build()
-    }
-
-    private fun iconTag(args: ArgumentQueue, ctx: Context): Tag {
-        val iconId = args.popOr("The <icon> tag requires an id to be passed.")
-            .value()
-
-        return Tag.selfClosingInserting(Component
-            .text(iconRegistry.mappedIcons[iconId] ?: 'f')
-        )
     }
 }
