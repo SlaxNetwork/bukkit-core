47aa465456516a9c43cc1a573644b8929ec764bd
Tech <debitcardzpaypal@gmail.com>
2023-03-18 21:41:53 -0400

    fix: unconverted property pt.2

--- api/src/main/kotlin/io/github/slaxnetwork/bukkitcore/minimessage/tags/IconTags.kt
+++ api/src/main/kotlin/io/github/slaxnetwork/bukkitcore/minimessage/tags/IconTags.kt
@@ -19,7 +19,7 @@ class IconTags(
 
         return Tag.selfClosingInserting(
             Component
-                .text(iconRegistry.mappedIcons[iconId] ?: UNICODE_WARNING_SYMBOL)
+                .text(iconRegistry.icons[iconId] ?: UNICODE_WARNING_SYMBOL)
         )
     }
 
