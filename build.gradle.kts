40e2e76978d78da953ec1aba1c50765ae1101e92
Tech <debitcardzpaypal@gmail.com>
2023-03-23 22:14:30 -0400

    misc: gradle changes

--- build.gradle.kts
+++ build.gradle.kts
@@ -21,18 +21,23 @@ allprojects {
     }
 
     dependencies {
+        compileOnly("io.papermc.paper:paper-api:1.19.4-R0.1-SNAPSHOT")
+
         implementation("io.github.slaxnetwork:kyouko-wrapper:0.0.1")
 
-        compileOnly("io.papermc.paper:paper-api:1.19.4-R0.1-SNAPSHOT")
+        api("com.github.shynixn.mccoroutine:mccoroutine-bukkit-api:2.11.0")
+        api("com.github.shynixn.mccoroutine:mccoroutine-bukkit-core:2.11.0")
     }
 }
 
 dependencies {
     implementation(project(":api"))
 
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib:1.8.10")
 
+    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.0")
     implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.0-Beta")
+    implementation("org.jetbrains.kotlin:kotlin-reflect:1.8.10")
 
     implementation("com.github.shynixn.mccoroutine:mccoroutine-bukkit-api:2.11.0")
     implementation("com.github.shynixn.mccoroutine:mccoroutine-bukkit-core:2.11.0")
